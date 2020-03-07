47443142bac83965d9862101e84b88ae38b19709
retroheim <60027238+retroheim@users.noreply.github.com>
2020-03-07 04:09:39 -0500

    Update Session.php

--- src/shoghicp/BigBrother/network/Session.php
+++ src/shoghicp/BigBrother/network/Session.php
@@ -236,7 +236,7 @@ class Session{
 				$serverPort = Binary::readShort(substr($buffer, $offset, 2));
 				$offset += 2;
 				$nextState = Binary::readComputerVarInt($buffer, $offset);
-				$protocol = ServerManager::PROTOCOL;
+				$protocol == ServerManager::PROTOCOL;
 				if($nextState === 1){
 					$this->status = 1;
 				}elseif($nextState === 2){
